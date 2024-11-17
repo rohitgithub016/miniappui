b7b8194b49e192cdbd9a7dc6ba1e5dfb10d23a6e
rohitgithub016 <rohitbhandari016@gmail.com>
2024-11-17 17:15:46 +0530

    asdf

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,46 @@
-import "./App.css"
-
+import "./App.css";
 
 const App = () => {
   return (
-    <div className='container'>App</div>
-  )
-}
+    <div className="container">
+      App
+      <div style={{ padding: "40px" }}>
+        <input placeholder="hello"></input>
+      </div>
+      <div style={{ padding: "40px" }}>
+        <input placeholder="hello"></input>
+      </div>
+      <div style={{ padding: "40px" }}>
+        <input placeholder="hello"></input>
+      </div>
+      <div style={{ padding: "40px" }}>
+        <input placeholder="hello"></input>
+      </div>
+      <div style={{ padding: "40px" }}>
+        <input placeholder="hello"></input>
+      </div>
+      <div style={{ padding: "40px" }}>
+        <input placeholder="hello"></input>
+      </div>
+      <div style={{ padding: "40px" }}>
+        <input placeholder="hello"></input>
+      </div>
+      <div style={{ padding: "40px" }}>
+        <input placeholder="hello"></input>
+      </div>
+      <div style={{ padding: "40px" }}>
+        <input placeholder="hello"></input>
+      </div>
+      <div style={{ padding: "40px" }}>
+        <input placeholder="hello"></input>
+      </div>
+      <div style={{ bottom: 0, position: "fixed", background: "red", width: '100%' }}>
+        <div style={{ padding: "20px" }}>
+          <button>click me</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
