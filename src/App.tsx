691ced5dfd3a9875a4a9c6c2a77587c2ccc47197
rohitgithub016 <rohitbhandari016@gmail.com>
2024-11-17 18:37:20 +0530

    asdf

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import "./style.css";
 
 export default function App() {
   WebApp.setBackgroundColor("#EFEFF4")
+  
   return (
     <div className="App" style={{height:'100vh', overflow: 'scroll'}}>
-      <div className="fix-bottom-button">I am fix at bottom</div>
+      <div className="fix-bottom-button" onClick={()=>alert("hello")}>I am fix at bottom</div>
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
       <br />
