c2d36861416de555ca6c237f2bccab2a5d6a6237
rohitgithub016 <rohitbhandari016@gmail.com>
2024-12-09 20:54:03 +0530

    sadfasdf

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import WebApp from "@twa-dev/sdk";
 import "./style.css";
+import { useEffect } from "react";
 
 WebApp.setBackgroundColor("#EFEFF4");
 WebApp.expand();
 WebApp.disableVerticalSwipes();
 
 export default function App() {
+  useEffect(() => {
+    WebApp?.BackButton?.show();
+    WebApp?.BackButton?.onClick(() => {
+      history.back();
+    });
+  }, []);
 
-  return (
-    <div>
-
-    </div>
-  );
+  return <div></div>;
 }
