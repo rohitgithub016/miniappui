e3646cd1708f5467a6bdf9048231d4f703ed0571
rohitgithub016 <rohitbhandari016@gmail.com>
2024-12-10 00:18:03 +0530

    asdf

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import WebApp from "@twa-dev/sdk";
 import "./style.css";
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 
 WebApp.setBackgroundColor("#EFEFF4");
 WebApp.expand();
 WebApp.disableVerticalSwipes();
 
 export default function App() {
-  const navigate = useNavigate();
+
   const handleBack = () => {
     console.log("parent");
     history.back();
@@ -18,7 +17,10 @@ export default function App() {
     WebApp?.BackButton?.onClick(handleBack);
     return () => WebApp?.BackButton.offClick(handleBack);
   }, []);
-  return <div>APP
-    <button onClick={()=>navigate("/home")}></button>
-  </div>;
+  return (
+    <div>
+      APP
+      {/* <button onClick={() => navigate("/home")}></button> */}
+    </div>
+  );
 }
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -15,7 +15,7 @@ const router = createBrowserRouter([
           element: <App />,
         },
         {
-          path: "/",
+          path: "/home",
           element: <Home />,
         },
         {
