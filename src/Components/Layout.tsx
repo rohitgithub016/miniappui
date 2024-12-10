a1e49f392a3d756562cb871f380aaf9943807d9c
rohitgithub016 <rohitbhandari016@gmail.com>
2024-12-10 11:12:27 +0530

    asdfasdf

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -1,20 +1,8 @@
-import WebApp from "@twa-dev/sdk";
 import eruda from "eruda";
-import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 
 eruda.init();
 const Layout = () => {
-
-  const handleBack = () => {
-    history.back();
-  };
-
-  useEffect(() => {
-    WebApp?.BackButton?.show();
-    WebApp?.BackButton?.onClick(handleBack);
-    return () => WebApp?.BackButton.offClick(handleBack);
-  }, []);
 
   return (
     <div>
