d80bae2dbc455c217b5a27681ccba76c1addec4d
rohitgithub016 <rohitbhandari016@gmail.com>
2025-02-19 15:38:10 +0530

    FONT TESTING

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 const App = () => {
   return (
-    <div style={{background: 'white'}}>
+    <div style={{ background: "white" }}>
       <h1>Header</h1>
+      <h2>Header</h2>
+      <h3>Header</h3>
+      <h4>Header</h4>
+      <h5>Header</h5>
 
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ipsum
