fd919ecbb9465baf6176993c936fec4150ca534f
rohitgithub016 <rohitbhandari016@gmail.com>
2025-03-19 22:10:08 +0530

    Changes fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,28 @@
-// import { Flex } from "antd";
-// import SupportIconSVG from "./assets/SupportIconSVG";
-// import ConnectWalletButton from "./Components/ConnectWalletButton";
-// import Dashboard from "./Components/Dashboard";
-// import DashboardBottomMenu from "./Components/DashboardBottomMenu";
-import WebApp from "@twa-dev/sdk";
-import { Flex } from "antd";
+
+import WebApp from '@twa-dev/sdk';
+import { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom'
 
 const App = () => {
-  const handle1 = () => {
-    WebApp.HapticFeedback.impactOccurred('medium')
-  }
-  const handle2 = () => {
-    WebApp.HapticFeedback.impactOccurred('heavy')
-  }
-  const handle3 = () => {
-    WebApp.HapticFeedback.notificationOccurred('success')
-  }
-  const handle4 = () => {
-    WebApp.HapticFeedback.notificationOccurred('warning')
-  } 
-   const handle5 = () => {
-    WebApp.HapticFeedback.notificationOccurred('error')
-  }
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    if(location?.pathname === "/about-us"){
+      WebApp?.BackButton?.onClick(()=>{
+        navigate("/");
+      })
+    } else {
+      WebApp?.BackButton?.onClick(()=>{
+        history?.back();
+      })
+    }
+  },[])
   return (
-    <Flex gap={20}>
-      <button onClick={handle1}> Medium</button>
-      <button onClick={handle2}> Heavy</button>
-      <button onClick={handle3}> Success</button>
-      <button onClick={handle4}> Warning</button>
-      <button onClick={handle5}> Error</button>
+    <div>
+      <button onClick={()=>navigate("/career")}>Career</button>
+    </div>
+  )
+}
 
-      {/* <Flex
-        vertical
-        style={{ overflow: "auto", height: "90vh" }}
-        id="container"
-      >
-        <button onClick={handleB}>Hello</button>
-        <Flex style={{ padding: "20px 20px 0px 20px" }}>
-          <Flex justify="space-between" style={{ width: "100%" }}>
-            <ConnectWalletButton />
-            <Flex
-              onClick={()=>undefined}
-              gap={4}
-              style={{
-                padding: "8px",
-                borderRadius: "8px",
-                border: "1px solid #DDD",
-                background: " #F8F8FA",
-              }}
-              align="center"
-            >
-              <SupportIconSVG />
-            </Flex>
-          </Flex>
-        </Flex>
-        {<Dashboard />}
-
-      </Flex>
-      <DashboardBottomMenu
-        selectedMenu={"home"}
-        handleChangeMenu={() => {}}
-      /> */}
-    </Flex>
-  );
-};
-
-export default App;
+export default App
--- src/Components/AboutUs.tsx
+++ src/Components/AboutUs.tsx
@@ -1,25 +1,12 @@
-import WebApp from "@twa-dev/sdk";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { increment } from "../store";
-
 const AboutUs = () => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-
-  const handleBack = () => {
-    console.log("child")
-    navigate("/home");
-  }
-  useEffect(() => {
-    dispatch(increment())
-    WebApp?.BackButton?.onClick(handleBack);
-    return () => {WebApp?.BackButton.offClick(handleBack)};
-  }, []);
-  return <div>
-    <button onClick={handleBack}>AboutUs</button>
-  </div>;
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, officia
+      quidem! Eius nam qui aperiam repellat quos doloribus laboriosam quibusdam
+      error at. Perferendis magnam repellat recusandae architecto itaque modi
+      cumque.
+    </div>
+  );
 };
 
 export default AboutUs;
