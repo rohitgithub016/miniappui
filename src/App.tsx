dae32f52302388b22643534c14d94326bde15410
rohitgithub016 <rohitbhandari016@gmail.com>
2025-03-21 00:04:54 +0530

    sadf

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,34 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
-const App = () => {
-  const inputRef = useRef<HTMLInputElement | null>(null);
+const MoveCursorToEnd = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [firstTime, setTime] = useState(true);
 
-  const handleFocus = () => {
-    console.log("hello");
+  const moveCursorToEnd = () => {
+    console.log("asdfasdf")
     if (inputRef.current) {
-      if (inputRef.current) {
-        inputRef.current.selectionStart = inputRef.current.selectionEnd =
-          inputRef.current.value.length;
-      }
+      const length = inputRef.current.value.length;
+      inputRef.current.focus();
+      inputRef.current.setSelectionRange(length, length);
     }
   };
 
+  console.log(firstTime)
+
   return (
-    <div>
-      <input
-        ref={inputRef}
-        onFocus={handleFocus}
-        defaultValue={2343}
-        type="number"
-        style={{ textAlign: "right" }}
+    <div className="flex flex-col items-center gap-4 p-4">
+      <input 
+        ref={inputRef} 
+        defaultValue="Type here..." 
+        style={{textAlign: 'end'}}
+        onClick={firstTime ? moveCursorToEnd : () => undefined}
+        onKeyDown={()=>setTime(false)} 
+        onBlur={()=>{
+          setTime(true)
+        }}
       />
     </div>
   );
 };
 
-export default App;
+export default MoveCursorToEnd;
