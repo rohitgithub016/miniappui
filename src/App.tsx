e98f5566abfab37e86cecd731ddee2813cb9bc41
yogiIO <dizizoggy@gmail.com>
2025-03-28 12:57:18 +0530

    input mode numeric

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ const MoveCursorToEnd = () => {
     <div className="flex flex-col items-center gap-4 p-4">
       <input 
       type="number"
+      inputmode="numeric"
         ref={inputRef} 
         defaultValue="Type here..." 
         style={{textAlign: 'end'}}
