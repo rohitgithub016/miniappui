828d4219c958b74671911ad28ca56f3037a9cdda
rohitgithub016 <rohitbhandari016@gmail.com>
2025-04-29 11:48:34 +0530

    asdf

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
+import WebApp from "@twa-dev/sdk";
+import { Flex } from "antd";
+import { useEffect } from "react";
+
 const App = () => {
+  useEffect(() => {
+    WebApp.ready();
+    if (WebApp) {
+      //WebApp.requestFullscreen();
+    }
+  }, []);
   return (
-    <div style={{width: "100vw", height: "100vh", overflow: "hidden"}}>
-      <video style={{objectFit: 'cover'}} src="./bg.mp4" autoPlay width={"100%"} height={"100%"} loop muted></video>
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        overflow: "hidden",
+        position: "relative",
+      }}
+    >
+      <video
+        style={{ objectFit: "cover" }}
+        src="./bg.mp4"
+        autoPlay
+        width={"100%"}
+        height={"100%"}
+        loop
+        muted
+      ></video>
+      <Flex vertical gap={16} style={{ position: "absolute", top: "50%", left: '35%' }}>
+        <img
+          src="./logo.png"
+          alt="Logo"
+          style={{ width: "50%", height: "100%",  }}
+        />
+      </Flex>
     </div>
   );
 };
