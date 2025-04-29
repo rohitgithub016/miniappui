c7e4c2c6a01549144862beef6e366094bf23221f
rohitgithub016 <rohitbhandari016@gmail.com>
2025-04-29 10:38:44 +0530

    asdf

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import WebApp from "@twa-dev/sdk";
 import { Button, Flex, Typography } from "antd";
- 
+import { useEffect } from "react";
 
 const MoveCursorToEnd = () => {
-
   console.log(import.meta.env.VITE_API_URL);
+
+  useEffect(() => {
+    WebApp?.requestFullscreen();
+  }, []);
+
   const handleOpen = () => {
     WebApp?.openTelegramLink("https://t.me/+UmFXFwHjYaQ4ZjY1");
     WebApp?.close();
-  }
+  };
 
   const handleOpen1 = () => {
     WebApp?.openLink("https://t.me/+UmFXFwHjYaQ4ZjY1");
     WebApp?.close();
-  }
+  };
 
   const handleOpen2 = () => {
     window?.open("https://t.me/+UmFXFwHjYaQ4ZjY1", "_blank");
     WebApp?.close();
-  }
+  };
 
   console.log(WebApp?.platform);
   return (
@@ -42,11 +46,17 @@ const MoveCursorToEnd = () => {
           similique ipsum accusamus doloribus molestias sunt itaque harum.
         </Typography.Paragraph>
 
-        <Button onClick={handleOpen} style={{color: 'red'}}>Click me</Button>
-        <Button onClick={handleOpen1} style={{color: "red"}}> Click me one</Button>
-        <Button onClick={handleOpen2} style={{color: "red"}}> Click me two</Button>
-
-
+        <Button onClick={handleOpen} style={{ color: "red" }}>
+          Click me
+        </Button>
+        <Button onClick={handleOpen1} style={{ color: "red" }}>
+          {" "}
+          Click me one
+        </Button>
+        <Button onClick={handleOpen2} style={{ color: "red" }}>
+          {" "}
+          Click me two
+        </Button>
       </Flex>
     </div>
   );
