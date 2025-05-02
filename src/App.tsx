b8164abdbdeafd35bce455be9748c80e63d17dd7
rohitgithub016 <rohitbhandari016@gmail.com>
2025-05-02 13:12:25 +0530

    Added the title

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,19 @@
 import WebApp from "@twa-dev/sdk";
-import { Flex } from "antd";
+import { Flex, Typography } from "antd";
 import { useEffect } from "react";
 
 const App = () => {
   useEffect(() => {
     WebApp.ready();
-    if (WebApp) {
+    if (Number(WebApp) >= 8) {
       WebApp?.expand();
       WebApp.requestFullscreen();
     }
   }, []);
   return (
-    <div
-      style={{
-        width: "100vw",
-        height: "100vh",
-        overflow: "hidden",
-        position: "relative",
-      }}
-    >
-      <video
-        style={{
-          objectFit: "cover",
-          width: "100%",
-          height: "100%",
-        }}
-        src="./splash.mp4"
-        autoPlay
-        loop
-        muted
-        playsInline
-      ></video>
-      <Flex
-        vertical
-        gap={16}
-        style={{ position: "absolute", top: "45%", width: "100%" }}
-        justify="center"
-        align="center"
-      >
-        <img
-          src="./logo.png"
-          alt="Logo"
-          style={{ width: "318px", height: "109px" }}
-        />
-      </Flex>
-    </div>
+    <Flex style={{padding: '10px'}}>
+      <Typography.Title level={3}>Hello, Telegram Web App!</Typography.t>
+    </Flex>
   );
 };
 
