6ffce951dcd77603a9e7ae9ac31d26e02e3ce78f
rohitgithub016 <rohitbhandari016@gmail.com>
2025-05-05 15:28:01 +0530

    Updated padding

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -26,7 +26,7 @@ const Layout = () => {
     if(WebApp?.platform === "tdesktop") {
       return '0px';
     } else if (WebApp?.platform === "android") {
-      return '20px';
+      return '0px';
     } else if (WebApp?.platform === "ios") { 
       return '110px';
     } else if (WebApp?.platform === "unknown") {
