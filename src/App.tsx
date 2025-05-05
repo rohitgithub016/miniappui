f14a97e25d4b29788c22272b23a185dd959e11cc
rohitgithub016 <rohitbhandari016@gmail.com>
2025-05-05 13:06:27 +0530

    Fixed the asdf

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,24 @@ const App = () => {
 
   console.log(WebApp?.safeAreaInset?.top);
   console.log(WebApp?.contentSafeAreaInset?.top);
-  
+
   return (
-    <Flex style={{padding: '20px', background: 'yellow'}}>
-      <Typography.Title style={{fontWeight: 700}} className="txt" level={2}>Hello, Telegram Web App!</Typography.Title>
+    <Flex style={{ padding: "20px", background: "yellow", overflow: 'scroll', height: '100vh' }} vertical >
+      <Typography.Title style={{ fontWeight: 700 }} className="txt" level={2}>
+        Hello, Telegram Web App!
+      </Typography.Title>
+      <Typography.Title style={{ fontWeight: 700 }} className="txt" level={1}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+        cupiditate, id necessitatibus laudantium fuga, soluta quam molestias
+        magni voluptatem, maxime quis labore porro nulla sapiente repellendus
+        aliquid nesciunt inventore. Excepturi.
+      </Typography.Title>
+      <Typography.Title style={{ fontWeight: 700 }} className="txt" level={1}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+        cupiditate, id necessitatibus laudantium fuga, soluta quam molestias
+        magni voluptatem, maxime quis labore porro nulla sapiente repellendus
+        aliquid nesciunt inventore. Excepturi.
+      </Typography.Title>
     </Flex>
   );
 };
--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -21,6 +21,7 @@ const Layout = () => {
       });
     }
   }, [location?.pathname]);
+  
   return (
     <div
       style={{
