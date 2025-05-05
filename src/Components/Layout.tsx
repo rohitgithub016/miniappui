b96398a98f093ca50495ce1a3c1695f749e9c93c
rohitgithub016 <rohitbhandari016@gmail.com>
2025-05-05 15:39:12 +0530

    asdf

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -26,7 +26,7 @@ const Layout = () => {
     if(WebApp?.platform === "tdesktop") {
       return '0px';
     } else if (WebApp?.platform === "android") {
-      return '110px';
+      return '80px';
     } else if (WebApp?.platform === "ios") { 
       return '110px';
     } else if (WebApp?.platform === "unknown") {
