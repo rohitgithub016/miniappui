71f6fc41811299c2b57c1b9c90e81561a81a301d
rohitgithub016 <rohitbhandari016@gmail.com>
2025-05-14 15:57:18 +0530

    FGHJK

--- src/App.tsx
+++ src/App.tsx
@@ -3,83 +3,62 @@ import { Button, Flex, Typography } from "antd";
 import { useEffect, useState } from "react";
 
 const App = () => {
-  const [viewportHeight, setViewportHeight] = useState(0);
-  const [v, setV] = useState(true);
-
-  useEffect(() => {
-    setViewportHeight(WebApp?.viewportStableHeight);
-  }, []);
+  const [isKeyboardOpen, setIsKeyboardOpen] = useState(false);
+  const [initialHeight, setInitialHeight] = useState(WebApp.viewportStableHeight);
 
   useEffect(() => {
     WebApp.ready();
     if (Number(WebApp?.version) >= 8 && WebApp?.platform !== "tdesktop") {
-      WebApp?.expand();
+      WebApp.expand();
       WebApp.requestFullscreen();
     }
-  }, []);
 
-  let heightChanged = WebApp?.viewportStableHeight < viewportHeight;
+    // Save the initial stable height
+    setInitialHeight(WebApp.viewportStableHeight);
 
-  const handleChange = () => {
-    console.log("Hello");
-  };
+    // Listen for viewport changes
+    WebApp.onEvent("viewportChanged", () => {
+      const stableHeight = WebApp.viewportStableHeight;
 
-  const handleClick = () => {
-    WebApp?.onEvent("viewportChanged", () => {
-      heightChanged = WebApp?.viewportStableHeight < viewportHeight;
-      console.log("viewportHeight", WebApp.viewportHeight);
-      console.log("viewportStableHeight", WebApp.viewportStableHeight);
-      if (WebApp?.viewportHeight === 852 || WebApp.viewportStableHeight == 852) {
-        setV(true);
-        console.log("inside");
-      }
-      setV(false);
+      // If height decreased significantly, keyboard likely opened
+      const keyboardOpened = stableHeight < initialHeight - 100;
+
+      setIsKeyboardOpen(keyboardOpened);
     });
-  };
-
-  console.log(v);
+  }, []);
 
   return (
     <Flex
       style={{
         padding: "20px",
         background: "yellow",
-        overflow: "scroll",
+        overflow: "auto",
         height: "100vh",
       }}
       vertical
     >
       <input type="text" />
-      <Typography.Title style={{ fontWeight: 700 }} className="txt" level={2}>
-        Hello, Telegram Web App!
+      <Typography.Title level={2}>Hello, Telegram Web App!</Typography.Title>
+      <input type="text" />
+      <Typography.Title level={1}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum cupiditate
       </Typography.Title>
       <input type="text" />
-      <Typography.Title style={{ fontWeight: 700 }} className="txt" level={1}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-        cupiditate
+      <Typography.Title level={1}>
+        More content here to allow scrolling and test keyboard push
       </Typography.Title>
-      <input
-        type="text"
-        onChange={handleChange}
-        onClick={handleClick}
-        onBlur={() => setV(true)}
-      />
-      <Typography.Title style={{ fontWeight: 700 }} className="txt" level={1}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-        cupiditate, id necessitatibus laudantium fuga, soluta quam molestias
-        magni voluptatem, maxime quis labore porro nulla sapiente repellendus
-        aliquid nesciunt inventore. Excepturi.
-      </Typography.Title>
-      <Flex
-        style={{
-          visibility: v ? "visible" : "hidden",
-          bottom: 20,
-          position: heightChanged ? "relative" : "fixed",
-          width: "calc(100% - 40px)",
-        }}
-      >
-        <Button style={{ width: "100%" }}>Click me</Button>
-      </Flex>
+
+      {!isKeyboardOpen && (
+        <Flex
+          style={{
+            bottom: 20,
+            position: "fixed",
+            width: "calc(100% - 40px)",
+          }}
+        >
+          <Button style={{ width: "100%" }}>Click me</Button>
+        </Flex>
+      )}
     </Flex>
   );
 };
