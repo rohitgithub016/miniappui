1d55cc11bc5810475df09c6cc2c24d146489cff2
rohitgithub016 <rohitbhandari016@gmail.com>
2025-05-26 15:24:10 +0530

    asdf

--- src/App.tsx
+++ src/App.tsx
@@ -1,81 +1,20 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import TabCarousel from "./Components/CaroselAnimation";
+import WebApp from "@twa-dev/sdk"
+import { useEffect } from "react"
+
 
 const App = () => {
-  const [viewportHeight, setViewportHeight] = useState<number | null>(null);
-  const [initialHeight, setInitialHeight] = useState<number | null>(null);
-  console.log(viewportHeight);
-
   useEffect(() => {
-    const handleResize = () => {
-      const height = window.visualViewport?.height || window.innerHeight;
+    // This is a placeholder for any side effects or initializations  
+    WebApp?.expand();
+    WebApp?.disableVerticalSwipes();
+    WebApp?.requestFullscreen();
+  }, [])
 
-      if (!initialHeight) {
-        setInitialHeight(height);
-        setViewportHeight(height);
-        return;
-      }
-
-      setViewportHeight(height);
-    };
-
-    window.visualViewport?.addEventListener("resize", handleResize);
-    handleResize(); // initial call
-
-    return () => {
-      window.visualViewport?.removeEventListener("resize", handleResize);
-    };
-  }, [initialHeight]);
-
+  console.log(WebApp?.contentSafeAreaInset);
+  console.log(WebApp?.safeAreaInset)
   return (
-    <>
-      <div className="flex flex-col items-center gap-4 p-4 h-screen">
-        <TabCarousel />
-        <input
-          inputMode="numeric"
-          type="tel"
-          defaultValue="Type here..."
-          style={{ textAlign: 'end' }}
-          // onClick={firstTime ? moveCursorToEnd : () => undefined}
-          // onKeyDown={() => setTime(false)}
-          // onBlur={() => {
-          //   setTime(true)
-          // }}
-        />
-        <div style={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: '8px'
-        }}>
-          <Link to={'/home'}>Home</Link>
-          <Link to={'/about-us'}>About Us</Link>
+    <div>App</div>
+  )
+}
 
-        </div>
-
-      </div>
-      <video
-        id="splash-screen-video"
-        src="./bg.mp4"
-        autoPlay
-        loop
-        muted
-        playsInline
-        webkit-playsinline
-        preload="auto"
-        style={{
-          position: "fixed",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%",
-          objectFit: "cover",
-          zIndex: -1,
-          pointerEvents: "none",
-        }}
-      ></video>
-    </>
-  );
-};
-
-export default App;
+export default App
