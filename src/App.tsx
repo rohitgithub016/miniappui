72407b59ac04e8b9367ff5b6ea89a9816178e760
rohitgithub016 <rohitbhandari016@gmail.com>
2025-05-26 15:43:26 +0530

    adf

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,46 @@
-import WebApp from "@twa-dev/sdk"
-import { useEffect } from "react"
-
+import WebApp from "@twa-dev/sdk";
+import { Button, Flex, Typography } from "antd";
+import { useEffect } from "react";
 
 const App = () => {
   useEffect(() => {
-    // This is a placeholder for any side effects or initializations  
-    WebApp?.expand();
-    WebApp?.disableVerticalSwipes();
-    WebApp?.requestFullscreen();
-  }, [])
+    // This is a placeholder for any side effects or initializations
+    if (Number(WebApp.version) >= 8) {
+      WebApp?.expand();
+      WebApp?.disableVerticalSwipes();
+      WebApp?.requestFullscreen();
+      console.log(WebApp?.contentSafeAreaInset);
+      console.log(WebApp?.safeAreaInset);
+    }
+  }, []);
 
-  console.log(WebApp?.contentSafeAreaInset);
-  console.log(WebApp?.safeAreaInset)
   return (
-    <div>App</div>
-  )
-}
+    <Flex vertical style={{padding: '10px', height: '100vh', overflowY: 'auto'}}>
+      <Typography.Title level={1}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae in ut
+        quis ducimus perferendis cum reprehenderit odio vel, aspernatur
+        deleniti! Cum quas odit repellendus delectus laborum omnis dolorem ad a.
+      </Typography.Title>
+      <input type="text" placeholder="Type something..." />
 
-export default App
+      <Typography.Title level={1}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae in ut
+        quis ducimus perferendis cum reprehenderit odio vel, aspernatur
+        deleniti! Cum quas odit repellendus delectus laborum omnis dolorem ad a.
+      </Typography.Title>
+      <input type="text" placeholder="Type something..." />
+      <Typography.Title level={1}>
+        Lorem ipsum dolor adipi
+      </Typography.Title>
+      <div
+        style={{ bottom: 30, position: "fixed", width: "100%", padding: 10 }}
+      >
+        <Button type="primary" style={{ width: "calc(100vw - 20px)" }}>
+          CLICK ME
+        </Button>
+      </div>
+    </Flex>
+  );
+};
+
+export default App;
--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -1,46 +1,13 @@
-import WebApp from "@twa-dev/sdk";
 import eruda from "eruda";
-import { useEffect } from "react";
-import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 eruda.init();
 const Layout = () => {
-  const location = useLocation();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    WebApp.BackButton.isVisible = true;
-    WebApp.BackButton.show();
-    if (location?.pathname === "/about-us") {
-      WebApp?.BackButton?.onClick(() => {
-        navigate("/");
-      });
-    } else {
-      WebApp?.BackButton?.onClick(() => {
-        history?.back();
-      });
-    }
-  }, [location?.pathname]);
-
-  const getPaddingTop = () => {
-    if(WebApp?.platform === "tdesktop") {
-      return '0px';
-    } else if (WebApp?.platform === "android") {
-      return '90px';
-    } else if (WebApp?.platform === "ios") { 
-      return '110px';
-    } else if (WebApp?.platform === "unknown") {
-      return '0px';
-    }
-  }
-
   return (
     <div
       style={{
         width: "100vw",
         height: "100vh",
-        paddingTop: getPaddingTop(),
-        // backgroundColor: "red",
       }}
     >
       <Outlet />
