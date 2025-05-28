a7be8a8727b98e0ff5e7c696c5d9857cee21a4c1
rohitgithub016 <rohitbhandari016@gmail.com>
2025-05-28 10:28:17 +0530

    reset files

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,36 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
-import Layout from "../Components/Layout";
-import Home from "../Components/Home";
-import Career from "../Components/Career";
-import AboutUs from "../Components/AboutUs";
 import App from "../App";
 
 const router = createBrowserRouter([
     {
       path: "/",
-      element: <Layout />,
-      children: [
-        {
-          path: "/",
-          element: <App />,
-        },
-        {
-          path: "/home",
-          element: <Home />,
-        },
-        {
-          path: "/about-us",
-          element: <AboutUs />,
-        },
-        {
-          path: "/career",
-          element: <Career />,
-        },
-        {
-          path: "/temp",
-          element: <div>hello temp</div>,
-        },
-      ],
+      element: <App />,
     },
   ]);
 
