5ace07f245b412c75ff3bd8a46cccd039f266b1a
rohitgithub016 <rohitbhandari016@gmail.com>
2025-06-08 23:33:24 +0530

    asdf

--- src/hooks/useInitiateTelegram.ts
+++ src/hooks/useInitiateTelegram.ts
@@ -11,7 +11,6 @@ const useInitiateTelegram = () => {
   console.log(WebApp?.contentSafeAreaInset);
   console.log(WebApp?.safeAreaInset);
   if (Number(WebApp.version) > 8) {
-    WebApp.requestFullscreen();
     WebApp.expand();
     WebApp.disableVerticalSwipes();
   }
