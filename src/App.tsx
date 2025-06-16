74f72d4bf3ed989aef3c3d40c7c9a43917b3a209
rohitgithub016 <rohitbhandari016@gmail.com>
2025-06-16 13:17:32 +0530

    Hello

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import WebApp from "@twa-dev/sdk";
 const App = () => {
   const handleClick = () => {
     WebApp.openLink(
-      `https://github.com/login/oauth/authorize?client_id=Ov23liiNDiRwhIj4MBW5&redirect_uri=https://miniappui.vercel.app/about&scope=user:email&prompt=consent`
+      `https://github.com/login/oauth/authorize?client_id=Ov23liiNDiRwhIj4MBW5&redirect_uri=https://t.me/hubz_dev_v2_bot/start?startapp&scope=user:email&prompt=consent`
     );
   };
   return (
