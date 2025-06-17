72c0c73bb927836ec9ea25c83f6de2d6d7fc4299
rohitgithub016 <rohitbhandari016@gmail.com>
2025-06-17 12:41:37 +0530

    asdfsdf

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import WebApp from "@twa-dev/sdk";
+import eruda from "eruda";
+eruda.init();
+
+const app_url = `https://t.me/miniappuibot?startapp=subscribe-684fc1087d6161139b0b11ed`
 
 const App = () => {
+  // useInitiateTelegram();
   const handleClick = () => {
     WebApp.openLink(
-      `https://github.com/login/oauth/authorize?client_id=Ov23liiNDiRwhIj4MBW5&redirect_uri=https://t.me/miniappuibot?startapp&scope=user:email&prompt=consent`
+      `https://github.com/login/oauth/authorize?client_id=Ov23liiNDiRwhIj4MBW5&redirect_uri=${app_url}&scope=user:email&prompt=consent`
     );
   };
-  console.log(WebApp?.initDataUnsafe?.start_param)
+  console.log(WebApp?.initDataUnsafe?.start_param);
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
       <button onClick={handleClick}>Click Me</button>
