22f2e79eed21e0188abd05293e36a3a9f1538b16
rohitgithub016 <rohitbhandari016@gmail.com>
2025-06-17 12:12:13 +0530

    Hello world

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import WebApp from "@twa-dev/sdk";
 const App = () => {
   const handleClick = () => {
     WebApp.openLink(
-      `https://github.com/login/oauth/authorize?client_id=Ov23liiNDiRwhIj4MBW5&redirect_uri=https://t.me/hubz_dev_v2_bot/start?startapp&scope=user:email&prompt=consent`
+      `https://github.com/login/oauth/authorize?client_id=Ov23liiNDiRwhIj4MBW5&redirect_uri=https://t.me/miniappuibot?startapp&scope=user:email&prompt=consent`
     );
   };
+  console.log(WebApp?.initDataUnsafe?.start_param)
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
       <button onClick={handleClick}>Click Me</button>
