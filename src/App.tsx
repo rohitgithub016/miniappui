7b3a45bb2da3b234788bd9f8013d11aa14b83277
rohitgithub016 <rohitbhandari016@gmail.com>
2025-06-26 11:11:14 +0530

    Hello

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
+import { Button, Modal } from "antd";
+import { useState } from "react";
+
 const App = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
   return (
-    <div style={{ width: "100%", height: "500px" }}>
-      <iframe
-        src="https://dashboard.hubz.io/"
-        title="Example Iframe"
-        style={{ width: "400px", height: "400px", border: "none" }}
-      ></iframe>
+    <div style={{ width: "100%", height: "100%" }}>
+      <Button type="primary" onClick={showModal}>
+        Open Modal
+      </Button>
+      <Modal
+        title="Basic Modal"
+        closable={{ "aria-label": "Custom Close Button" }}
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <iframe
+          src="https://dashboard.hubz.io/"
+          title="Example Iframe"
+          style={{ width: "100%", height: "100%", border: "none" }}
+        ></iframe>
+      </Modal>
     </div>
   );
 };
