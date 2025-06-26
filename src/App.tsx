95973f2b1e1af55721fa2f2f0e029397772ac11a
rohitgithub016 <rohitbhandari016@gmail.com>
2025-06-26 10:19:15 +0530

    Hello

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ const App = () => {
   return (
     <div style={{ width: "100%", height: "500px" }}>
       <iframe
-        src="https://www.w3schools.com"
+        src="https://www.example.com"
         title="W3Schools Free Online Web Tutorials"
       ></iframe>
     </div>
