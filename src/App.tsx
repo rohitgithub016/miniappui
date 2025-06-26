20ac9caff4aeb9ee2ed8051f09ef9b04f92bbb7d
rohitgithub016 <rohitbhandari016@gmail.com>
2025-06-26 10:53:58 +0530

    aa

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
+import { Typography } from "antd";
+
 const App = () => {
   return (
     <div style={{ width: "100%", height: "500px" }}>
-      <iframe
-        src="https://www.example.com"
-        title="W3Schools Free Online Web Tutorials"
-      ></iframe>
+      <Typography.Title>Hello World</Typography.Title>
     </div>
   );
 };
