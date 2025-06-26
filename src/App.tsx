a35b66ad7b39bd4f16ca1a033cdbac22cdb1b454
rohitgithub016 <rohitbhandari016@gmail.com>
2025-06-26 10:17:04 +0530

    testing iframe

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import { BrowserRouter as Router } from "react-router-dom";
-import NavBar from "./component/NavBar";
-import AnimatedRoutes from "./component/AnimatedRoutes";
-
 const App = () => {
-
   return (
-    <Router>
-      <NavBar />
-      <AnimatedRoutes/>
-    </Router>
+    <div style={{ width: "100%", height: "500px" }}>
+      <iframe
+        src="https://www.w3schools.com"
+        title="W3Schools Free Online Web Tutorials"
+      ></iframe>
+    </div>
   );
 };
 
