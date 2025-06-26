bad9684df7df332880521dfb6d2d146cd30f78ac
rohitgithub016 <rohitbhandari016@gmail.com>
2025-06-26 10:58:43 +0530

    Hello world

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { Typography } from "antd";
-
 const App = () => {
   return (
     <div style={{ width: "100%", height: "500px" }}>
-      <Typography.Title>Hello World</Typography.Title>
+      <iframe
+        src="https://dashboard.hubz.io/"
+        title="Example Iframe"
+        style={{ width: "400px", height: "400px", border: "none" }}
+      ></iframe>
     </div>
   );
 };
