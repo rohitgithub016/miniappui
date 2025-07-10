c910f8525e25a4f5ee12bba63ec473643acf6196
rohitgithub016 <rohitbhandari016@gmail.com>
2025-07-10 16:23:03 +0530

    Removed unsused file

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,5 @@
-import { Button, Modal } from "antd";
-import { useState } from "react";
-
 const App = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const showModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleOk = () => {
-    setIsModalOpen(false);
-  };
-
-  const handleCancel = () => {
-    setIsModalOpen(false);
-  };
-  return (
-    <div style={{ width: "100%", height: "100%" }}>
-      <Button type="primary" onClick={showModal}>
-        Open Modal
-      </Button>
-      <Modal
-        title="Basic Modal"
-        closable={{ "aria-label": "Custom Close Button" }}
-        open={isModalOpen}
-        onOk={handleOk}
-        onCancel={handleCancel}
-      >
-        <iframe
-          src="https://resources.hubz.io/badge-rewards-with-csbts/setting-up-badge-rewards-with-csbts"
-          title="Example Iframe"
-          style={{ width: "100%", height: "100%", border: "none" }}
-        ></iframe>
-      </Modal>
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
